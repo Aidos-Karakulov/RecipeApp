83f92b02ad8b4a0bfa8dd461da0aedb78d097780
Aidos Karakulov <codeakikulov@gmail.com>
2023-12-13 11:24:14 +0600

    First commit create introduction

---
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.recipeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+    }
+}
