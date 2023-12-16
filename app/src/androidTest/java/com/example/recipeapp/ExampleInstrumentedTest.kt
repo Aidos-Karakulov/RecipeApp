dba69fb5e6b86e446c20bfcaa36511f526569512
Omargali Tlepbergenov <210107016@stu.sdu.edu.kz>
2023-12-16 17:50:14 +0600

    Add category_bottom_fragment.xml and changed CategoryFragment.kt

--- app/src/androidTest/java/com/example/recipeapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/recipeapp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.easyfood", appContext.packageName)
+        assertEquals("com.example.recipeapp", appContext.packageName)
     }
 }
